a92b3a8029007c68fdfe6d2c2141be8ce0f982cd
wolfdud3 <45672884+wolfdud3@users.noreply.github.com>
2020-04-08 01:02:30 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  SudokuProblem
+//
+//  Created by wolfdud3 on 19/03/2020.
+//  Copyright © 2020 Robin Schulz. All rights reserved.
+//
+
+#include <iostream>
+#include "grid.h"
+#include "check.h"
+
+int main()
+{
+    std::cout << "main started" << std::endl;
+    
+    //function to load sudoku, found in grid.h
+    
+    
+};
